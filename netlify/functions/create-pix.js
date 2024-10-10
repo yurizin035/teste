bfc55c1f44425f52bdf317c9b15c5dcbe5da06c9
yuri <85718637+yurizin035@users.noreply.github.com>
2024-10-10 18:05:35 -0300

    Update create-pix.js

--- netlify/functions/create-pix.js
+++ netlify/functions/create-pix.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 exports.handler = async function(event, context) {
     const { valor, descricao } = JSON.parse(event.body);
